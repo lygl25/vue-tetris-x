379b3b338f7f96312543042586cdb54fe1f764ee
lygl25 <lygl25@163.com>
2020-04-25 10:34:54 +0800

    init vue-tetris-x

---
+++ src/components/keyboard/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div
+        class="keyboard"
+        :style="'margin-top:'+fillingNum+'px'"
+      >
+        <vbutton
+          color="blue"
+          size="s3"
+          :top="60"
+          :left="420"
+          :label="rotation"
+          :position="true"
+          :active="keyboard['rotate']"
+          ref="dom_rotate"
+        />
+        <vbutton
+          color="blue"
+          size="s0"
+          :top="180"
+          :left="330"
+          :label="labelDown"
+          :active="keyboard['down']"
+          ref="dom_down"
+        />
+        <vbutton
+          color="blue"
+          size="s1"
+          :top="90"
+          :left="4"
+          :label="labelLeft"
+          arrow="translate(60px, -12px) rotate(270deg)"
+          :active="keyboard['left']"
+          ref="dom_left"
+        />
+        <vbutton
+          color="blue"
+          size="s1"
+          :top='90'
+          :left='204'
+          :label="labelRight"
+          arrow="translate(-60px, -12px) rotate(90deg)"
+          :active="keyboard['right']"
+          ref="dom_right"
+        />
+        <vbutton
+          color="blue"
+          size="s1"
+          :top="190"
+          :left="105"
+          :label="labelDropSpace"
+          arrow="translate(-48px,-71px) rotate(180deg)"
+          :active="keyboard['drop']"
+          ref="dom_space"
+        />
+        <vbutton
+          color="red"
+          size="s2"
+          :top="0"
+          :left="236"
+          :label="labelResetR"
+          :active="keyboard['reset']"
+          ref="dom_r"
+        />
+        <vbutton
+          color="green"
+          size="s2"
+          :top="0"
+          :left="146"
+          :label="labelSoundS"
+          :active="keyboard['music']"
+          ref="dom_s"
+        />
+        <vbutton
+          color="green"
+          size="s2"
+          :top="0"
+          :left="56"
+          :label="labelPauseP"
+          :active="keyboard['pause']"
+          ref="dom_p"
+        />
+      </div>
+</template>
+<style lang="less">
+ @import './index.less';
+</style>
+
+<script src="./index.js">
+</script>
